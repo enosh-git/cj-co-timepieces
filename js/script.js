26688adabac707d4d11921e2c7c6c71cf41527d0
enosh-git <enoshsgmt01@gmail.com>
2026-05-12 16:50:44 +0800

    Add header structure and CSS to all pages

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ function switchImage(clickedThumbnail) {
     let mainImage = document.getElementById("mainImg");
     mainImage.src = clickedThumbnail.src;
 }
+
+document.addEventListener("scroll", function() {
+    if (window.scrollY >= 120) {
+        document.querySelector("header").classList.add("scrolled");
+    } else {
+        document.querySelector("header").classList.remove("scrolled");
+    }
+})
